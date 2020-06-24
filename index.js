a49f1d85f13102461c02c07c2321dd24f7a617c2
db <talktodonnybronson@gmail.com>
2020-06-24 17:51:07 +0200

    style updates, map recentered

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-
+ app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
---
+++ shortlink/index.php
@@ -0,0 +1,1 @@
+<?php  header( 'Location: https://www.google.com/maps/d/u/0/edit?mid=1bi0mGzb_pdyq36lalPlOjsdjtPnwLKon&ll=51.464561512576424%2C-2.5746802221679688&z=13' ) ;  ?>
